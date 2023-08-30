5075ac60e47fe099559df93ed21a7e4470bade94
rogergelioo <rogergeliotorres@gmail.com>
2023-08-30 11:05:19 -0600

    radar chart added

--- components/Charts/ChartThree.tsx
+++ components/Charts/ChartThree.tsx
@@ -1,40 +1,70 @@
 import { ApexOptions } from "apexcharts";
 import React, { useState } from "react";
 import dynamic from "next/dynamic";
+import { timeStamp } from "console";
 const ReactApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 interface ChartThreeState {
-  series: number[];
+  series: any;
 }
 
 const options: ApexOptions = {
   chart: {
-    type: "donut",
+    type: "radar",
+    toolbar: {
+      show: true,
+      offsetX: -100,
+      offsetY: 0,
+      tools: {
+        download: true,
+        selection: true,
+        pan: true,
+      },
+      export: {
+        csv: {
+          filename: undefined,
+          columnDelimiter: ',',
+          headerCategory: 'category',
+          headerValue: 'value',
+          dateFormatter(timestamp) {
+            return new Date(Date.now()).toDateString()
+          }
+        },
+        svg: {
+          filename: undefined,
+        },
+        png: {
+          filename: undefined,
+        }
+      },
+      autoSelected: 'zoom' 
+    },
   },
-  colors: ["#10B981", "#375E83", "#259AE6", "#FFA70B"],
-  labels: ["Remote", "Hybrid", "Onsite", "Leave"],
+  colors: ["#FF80AF", "#a799ff"],
   legend: {
     show: true,
     position: "bottom",
   },
-
   plotOptions: {
-    pie: {
-      donut: {
-        size: "65%",
-        background: "transparent",
-      },
-    },
+    radar: {
+      polygons: {
+        strokeColors: "#2E3A47",
+        fill: {
+            colors: ['#1A222C', '#24303F']
+        }
+      }
+    }
   },
   dataLabels: {
     enabled: false,
   },
+
   responsive: [
     {
       breakpoint: 2600,
       options: {
         chart: {
-          width: 380,
+          width: 600,
         },
       },
     },
@@ -47,11 +77,26 @@ const options: ApexOptions = {
       },
     },
   ],
+  xaxis: {
+    categories: ['Goals Scored', 'XG', 'Goals Conceded', 'XA', 'PPDA', 'Passes per game'],
+  },
+  yaxis: {
+    show: false,
+  },
 };
 
 const ChartThree: React.FC = () => {
   const [state, setState] = useState<ChartThreeState>({
-    series: [65, 34, 12, 56],
+    series: [
+      {
+        name: "Oakland Roots",
+        data: [80, 50, 30, 40, 100, 20]
+      },
+      {
+        name: "USL Average",
+        data: [20, 30, 40, 80, 20, 80]
+      },
+    ],
   });
 
   return (
@@ -59,7 +104,7 @@ const ChartThree: React.FC = () => {
       <div className="mb-3 justify-between gap-4 sm:flex">
         <div>
           <h5 className="text-xl font-semibold text-black dark:text-white">
-            Visitors Analytics
+            Offensive performance
           </h5>
         </div>
         <div>
@@ -101,49 +146,11 @@ const ChartThree: React.FC = () => {
           <ReactApexChart
             options={options}
             series={state.series}
-            type="donut"
+            type="radar"
           />
         </div>
       </div>
 
-      <div className="-mx-8 flex flex-wrap items-center justify-center gap-y-3">
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-primary"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Desktop </span>
-              <span> 65% </span>
-            </p>
-          </div>
-        </div>
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#6577F3]"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Tablet </span>
-              <span> 34% </span>
-            </p>
-          </div>
-        </div>
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#8FD0EF]"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Mobile </span>
-              <span> 45% </span>
-            </p>
-          </div>
-        </div>
-        <div className="w-full px-8 sm:w-1/2">
-          <div className="flex w-full items-center">
-            <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#0FADCF]"></span>
-            <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Unknown </span>
-              <span> 12% </span>
-            </p>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
