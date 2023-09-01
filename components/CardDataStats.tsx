25dbb10e55c670acf5de5b9d530a82ad10688afd
rogergelioo <rogergeliotorres@gmail.com>
2023-08-31 21:43:03 -0600

    aaa

--- components/CardDataStats.tsx
+++ components/CardDataStats.tsx
@@ -19,10 +19,6 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({
 }) => {
   return (
     <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
-      <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
-        {children}
-      </div>
-
       <div className="mt-4 flex items-end justify-between">
         <div>
           <h4 className="text-title-md font-bold text-black dark:text-white">
